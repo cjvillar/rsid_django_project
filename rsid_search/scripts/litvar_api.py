c323d675128e666c7db6cf07b270023ffc3a1ed7
Chris Villarreal <chrisvillarreal84@gmail.com>
2022-08-04 17:12:24 -0700

    updating litvar api

--- rsid_search/scripts/litvar_api.py
+++ rsid_search/scripts/litvar_api.py
@@ -5,6 +5,7 @@ import requests
 import argparse
 
 sys.path.append("/Users/chrisv/rsid_django_project/")
+sys.path.append("rsid_django_project/")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rsid_search.settings")
 import django
 
@@ -13,6 +14,14 @@ django.setup()
 from rsid_catalog.models import Rsids, User
 
 
+def rs_from_file(infile):
+    rs_list = []
+    if infile is not None:
+        with open(infile,"r") as f:
+            for line in f:
+                rs_list.append(line.rstrip("\r\n"))
+        return rs_list
+        
 def litvar_url(rs, username):
     user = User.objects.get(username=username)
     url = "https://www.ncbi.nlm.nih.gov/research/bionlp/litvar/api/v1/entity/litvar/{}%23%23".format(
@@ -34,12 +43,17 @@ def litvar_url(rs, username):
     except ValueError as e:
         print(e)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("rs")
-    parser.add_argument("username")
+    parser.add_argument('--infile', help='input file, with rsIDs on each line')
+    parser.add_argument('--rs')
+    parser.add_argument('username')
     arguments = parser.parse_args()
     username = arguments.username
     rs = arguments.rs
+    infile = arguments.infile
+    rs_list = rs_from_file(infile)
+    if rs_list != None:
+        for rs in rs_list:
+            print(litvar_url(rs, username))
     print(litvar_url(rs, username))
