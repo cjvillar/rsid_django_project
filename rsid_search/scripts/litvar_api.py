960c13e9bdf26735c797a2b67d46ad30077ad162
Chris Villarreal <chrisvillarreal84@gmail.com>
2022-08-22 13:34:48 -0700

    update litvar_api.py

--- rsid_search/scripts/litvar_api.py
+++ rsid_search/scripts/litvar_api.py
@@ -3,57 +3,58 @@ import os, sys
 from re import U
 import requests
 import argparse
+import logging
 
 sys.path.append("/Users/chrisv/rsid_django_project/")
-sys.path.append("rsid_django_project/")
+sys.path.append("./rsid_django_project/")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rsid_search.settings")
 import django
 
 django.setup()
+logging.basicConfig(level=logging.INFO)
 
 from rsid_catalog.models import Rsids, User
 
 
-def rs_from_file(infile):
+def rs_from_file(rs_ids):
     rs_list = []
-    if infile is not None:
-        with open(infile,"r") as f:
-            for line in f:
-                rs_list.append(line.rstrip("\r\n"))
-        return rs_list
+    with open(rs_ids,"r") as f:
+        for line in f:
+            rs_list.append(line.rstrip("\n"))
+    return rs_list
         
-def litvar_url(rs, username):
+def litvar_url(rs_list, username):
     user = User.objects.get(username=username)
-    url = "https://www.ncbi.nlm.nih.gov/research/bionlp/litvar/api/v1/entity/litvar/{}%23%23".format(
-        rs
-    )
-    response = requests.get(url)
-    try:
-        data = response.json()
-        rs_id = data["id"]
-        gene = data["gene"]
-        diseases = data["diseases"]
-        clean_id = rs_id.strip("##")
-        new_id = Rsids.objects.create(
-            rs_id=clean_id, diseases=diseases, gene=gene, user=user
+    print(rs_list)
+    for rs_id in rs_list:
+        url = "https://www.ncbi.nlm.nih.gov/research/bionlp/litvar/api/v1/entity/litvar/{}%23%23".format(
+            rs_id
         )
-        new_id.save()
-
-        return rs_id, diseases, response, gene, data.keys()
-    except ValueError as e:
-        print(e)
+        response = requests.get(url)
+        try:
+            data = response.json()
+            rs_id = data["id"]
+            gene = data["gene"]
+            diseases = data["diseases"]
+            clean_id = rs_id.strip("##")
+            new_id = Rsids.objects.create(
+                rs_id=clean_id, diseases=diseases, gene=gene, user=user
+            )
+            new_id.save()
+            
+            logging.info(f'{rs_id}, {diseases}, {response}, {gene}.')
+            
+        except ValueError as e:
+            print(e)
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--infile', help='input file, with rsIDs on each line')
-    parser.add_argument('--rs')
+    parser.add_argument('rs_list', help=' an input file with rsIDs on each line')
     parser.add_argument('username')
     arguments = parser.parse_args()
     username = arguments.username
-    rs = arguments.rs
-    infile = arguments.infile
-    rs_list = rs_from_file(infile)
-    if rs_list != None:
-        for rs in rs_list:
-            print(litvar_url(rs, username))
-    print(litvar_url(rs, username))
+    rs_ids = arguments.rs_list
+    rs_list = rs_from_file(rs_ids)
+    print(litvar_url(rs_list , username))
+   
